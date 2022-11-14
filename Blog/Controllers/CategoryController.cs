f83fc00b1be794684a7d9ce33edf19e27d1bf3ae
Lucas Rodrigues <rodrigues14set@hotmail.com>
2022-11-13 22:16:56 -0300

    Iniciando o versionamento da API - Curso Balta.io

---
+++ Blog/Controllers/CategoryController.cs
@@ -0,0 +1,15 @@
+using Blog.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers;
+
+[ApiController]
+public class CategoryController : ControllerBase
+{
+    [HttpGet("v1/categories")]
+    public IActionResult Get([FromServices] BlogDataContext context)
+    {
+        var categories = context.Categories.ToList();
+        return Ok();
+    }
+}
