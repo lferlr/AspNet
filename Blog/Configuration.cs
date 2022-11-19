f557781cc542739afd4ade474d1e627ac87cb146
Lucas Rodrigues <rodrigues14set@hotmail.com>
2022-11-19 20:00:42 -0300

    finalizando o módulo de authorização e autenticação

---
+++ Blog/Configuration.cs
@@ -0,0 +1,8 @@
+namespace Blog;
+
+public static class Configuration
+{
+    public static string JwtKey = "mRm1IWV3PkawdgBiVz6dsw==";
+    public static string ApiKeyName = "api_key";
+    public static string ApiKey = "curso_api_mRm1IWV3Pk==awdgBiVz6dsw";
+}
--- Blog/Program.cs
+++ Blog/Program.cs
@@ -1,15 +1,42 @@
+using System.Text;
+using Blog;
 using Blog.Data;
+using Blog.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false
+        
+    };
+});
 
 builder.Services.AddControllers().ConfigureApiBehaviorOptions(options =>
 {
     options.SuppressModelStateInvalidFilter = true;
 });
 builder.Services.AddDbContext<BlogDataContext>();
+builder.Services.AddTransient<TokenService>(); // Sempre cria uma nova instancia para o Service
+//builder.Services.AddScoped(); // Ele dura por transação
+//builder.Services.AddSingleton(); // Singleton -> 1 por App! Sempre a mesma instância
 
 var app = builder.Build();
 
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
